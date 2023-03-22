f20174c06d3946088d254d925eb0d830726f5c28
AnonymousWP <50231698+AnonymousWP@users.noreply.github.com>
2023-03-22 21:36:18 +0100

    refactor(graylog2thehive): add environment variables

--- graylog2thehive/config.py
+++ graylog2thehive/config.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     API_KEY=os.environ.get('HIVE_SECRET_KEY')
-    HIVE_URL=''
-    LOG_FILE='/var/log/graylog2thehive.log'
-    GRAYLOG_URL=''
+    HIVE_URL=os.environ.get('HIVE_URL')
+    LOG_FILE='/graylog2thehive/graylog2thehive.log'
+    GRAYLOG_URL=os.environ.get('GRAYLOG_URL')
