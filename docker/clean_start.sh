7301f12e93f3f7a34a759abd5615549698ec67a8
AnonymousWP <50231698+AnonymousWP@users.noreply.github.com>
2023-04-04 09:51:48 +0200

    ops(docker): use one-liner to prune Docker resources

--- docker/clean_start.sh
+++ docker/clean_start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# This shell script stops the containers, then deletes them, including the images and volumes. In case executing this script doesn't work, allow it to execute: `sudo chmod +x ./clean_start.sh`
+# This shell script deletes all Docker data such as containers, networks, images and volumes. In case executing this script doesn't work, allow it to execute: `sudo chmod +x ./clean_start.sh`
 
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker rmi -f $(docker images -aq)
-docker volume rm $(docker volume ls -q)
+docker system prune -a -f --volumes
